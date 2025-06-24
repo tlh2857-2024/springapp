0714b262378580b70974f2ba6764a42c5954f194
tlh2857-2024 <terryleehillis+xsiam@gmail.com>
2025-06-24 18:15:39 -0500

    Update update.sh

--- app/update.sh
+++ app/update.sh
@@ -1,10 +1,10 @@
 echo "deb http://archive.debian.org/debian/ stretch main contrib non-free" > /etc/apt/sources.list \
 && echo "deb http://archive.debian.org/debian-security/ stretch/updates main" >> /etc/apt/sources.list \
 && apt-get update \
-&& apt-get install -y nmap \
-&& apt-get install -y curl \
-&& apt-get install -y wget \
-&& apt-get install -y git \
-&& apt-get install -y awscli \
-&& apt-get install -y mysql-client \
+&& apt-get install -y nmap --allow-unauthenticated \
+&& apt-get install -y curl --allow-unauthenticated \
+&& apt-get install -y wget --allow-unauthenticated \
+&& apt-get install -y git --allow-unauthenticated \
+&& apt-get install -y awscli --allow-unauthenticated \
+&& apt-get install -y mysql-client --allow-unauthenticated \
 && rm -rf /var/lib/apt/lists/* 
